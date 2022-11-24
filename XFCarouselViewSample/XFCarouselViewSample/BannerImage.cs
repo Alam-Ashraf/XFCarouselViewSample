60fa73c52b965b3125662f412cef3ca834beb84b
alamgeer <alamgeer06@gmail.com>
2022-11-24 21:57:33 +0530

    Commit changes

---
+++ XFCarouselViewSample/XFCarouselViewSample/BannerImage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XFCarouselViewSample
+{
+    public class BannerImage
+    {
+        public ImageSource ImageUrl { get; set; }
+        public string ImageName { get; set; }
+        public string ImageDesc { get; set; } = "Lorem Ipsum is simply dummy text of the printing and typesetting industry";
+        public bool IsSquareView { get; set; }
+        public Color HeaderColor { get; set; }
+        public bool IsLoading = true;
+    }
+}
+
---
+++ XFCarouselViewSample/XFCarouselViewSample/CarouselViewTemplateSelector.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XFCarouselViewSample
+{
+    public class CarouselViewTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate CurveTemplate { get; set; }
+        public DataTemplate SquareTemplate { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            BannerImage banner = (BannerImage)item;
+            return banner.IsSquareView ? SquareTemplate : CurveTemplate;
+        }
+    }
+}
+
